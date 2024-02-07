4c36fc52ae74cdca9329468ae83ac1c7424b9252
Raduica Iulian <raduicaiulian@gmail.com>
2024-02-07 21:22:09 +0200

    improved get_samples.sh

--- get_samples.sh
+++ get_samples.sh
@@ -2,6 +2,5 @@ url="http://ciprianpungila.com/uvt/sc/2023/samples"
 password="<archive_password>"
 
 for ((i=1;i<=25;i++)); do
-    wget  "$url$i.zip" && mkdir "sample$i" && mv "samples$i.zip" "sample$i/" && cd "sample$i/" && unzip -P $password "samples$i.zip" && cd .. &
-    #echo  "$url$i.zip" &
+    wget  "$url$i.zip" && mkdir "sample$i" && mv "samples$i.zip" "sample$i/" && cd "sample$i/" && unzip -P $password "samples$i.zip" && cd .. && rm "sample$i/samples$i.zip"&
 done
